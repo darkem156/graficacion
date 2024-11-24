a9bb139d9f373584513edf1d8a29586d701d6ff7
Emmanuel <emmanuelchan809@gmail.com>
2024-11-24 16:23:02 -0500

    Fixed base in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://darkem156.github.io/graficacion',
   plugins: [react()],
 })
